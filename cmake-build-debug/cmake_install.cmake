919d933db1740b291004be53a74699202f09f20d
L1za-T <78148912+L1za-T@users.noreply.github.com>
2024-04-30 00:56:26 +0100

    Final commit
    I think this one only happened from changing the folder name.

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/L1za/Documents/C++/bug_life_project
+# Install script for directory: C:/Users/L1za/Documents/C++/Pavlovas_Linas_CA3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/L1za/Documents/C++/bug_life_project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/L1za/Documents/C++/Pavlovas_Linas_CA3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
